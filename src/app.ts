6246eae917c6e211e102dda0042cab493183942e
mgravemeyer <m.gravemeyer@icloud.com>
2021-04-27 10:43:28 +0200

    added basic routing

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,23 @@
-import { createServer, IncomingMessage, ServerResponse } from 'http';
+// import { createServer, IncomingMessage, ServerResponse } from 'http';
 
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var express = require('express'), app = express(), port = process.env.PORT || 3000;
 
-const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-});
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+})
+
+app.get('/list', (req, res) => {
+    res.send('Here is the list!')
+})
+
+app.post('/list', (req, res) => {
+    res.send('Added Item in List!')
+})
+
+console.log('API started on: ' + port)
