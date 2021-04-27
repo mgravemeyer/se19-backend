7a799986b43335f87d61a125e501d09c253a6b58
mgravemeyer <m.gravemeyer@icloud.com>
2021-04-27 14:38:02 +0200

    added mongodb connection

--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,9 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`App listening at http://localhost:${port}`)
+// })
 
 app.get('/list', (req, res) => {
     res.send('Here is the list!')
@@ -21,3 +21,22 @@ app.post('/list', (req, res) => {
 })
 
 console.log('API started on: ' + port)
+
+//mongodb connection
+
+const findItems = async () => {
+    const MongoClient = require('mongodb').MongoClient;
+    const uri = "mongodb+srv://test-user:Jnsgc7y.EWM4X@se19cluster.8tdv4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    const connect = await client.connect(err => {
+        const collection = client.db("test").collection("devices").insertOne({'test': "test"})
+            .then(result => {
+                console.log(result)
+                client.close();
+            })
+        // perform actions on the collection object
+    })
+};
+
+findItems();
+
