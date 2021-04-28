f5c5c564575ce00bd9c65ff73b72a9fe589bfb2b
mgravemeyer <m.gravemeyer@icloud.com>
2021-04-28 10:02:27 +0200

    changed / enpoint text

--- app.ts
+++ app.ts
@@ -23,7 +23,7 @@ client.connect();
 client.on('error', console.error.bind(console, 'MongoDB connection error:'));
 //-------------- routes --------------
 app.get('/', (req, res) => {
-    res.send('Hello World! Available endpoints: </br><strong>\\list {get}</br>\\listAdd {post: name}</br>\\listRemoveAll {get}</br>\\listRemove {post: id}</strong>');
+    res.send('Hello World! Available endpoints: </br><strong>\\list {get}</br>\\listAdd {post: name}</br>\\listRemoveItem {post: id}</br>\\listRemoveAll {get}</strong>');
 })
 
 app.get('/list', (req, res) => {
